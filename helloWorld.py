14c747ca7b9be5ec4d90e2ee49018da815b4720f
jsonfrench <114097230+jsonfrench@users.noreply.github.com>
2022-09-26 12:51:11 -0400

    This is version 2 of my hello world program

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,2 @@
-# This is Version 1 of my hello world program
-print("Hello my name is insert your first name") 
+# This is Version 2 of my hello world program
+print("Hello my name is insert your first name and last name") 
